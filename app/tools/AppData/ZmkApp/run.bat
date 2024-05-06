d483f9c6454894cbdf840b5949475c531de4ab06
dariel_zhang <dariel_zhang@realsil.com.cn>
2024-05-06 16:19:06 +0800

    APP-keyboard: optimize independent dfu
    1. update prepend header version
    2. modify run.bat&mp.ini

--- app/tools/AppData/ZmkApp/run.bat
+++ app/tools/AppData/ZmkApp/run.bat
@@ -1,5 +1,6 @@
+del "zmk*.bin"
 cp ..\..\..\build\zephyr\zmk.bin .
-..\..\prepend_header\prepend_header.exe -t secure_app_data -p "zmk.bin" -m 1 -c sha256 -b 15
+..\..\prepend_header\prepend_header.exe -t app_code -p "zmk.bin" -m 1 -c sha256 -b 15
 1>NUL ren zmk.tmp zmkImage.bin
 1>NUL ren zmk_MP.bin zmkImage_MP.bin
 ..\..\md5\md5.exe "zmkImage_MP.bin"
